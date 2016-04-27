118fe31618680307b36b2ae7d33385b56bd60b31
Srinivas Avireddy <sriniavireddy@gmail.com>
2016-04-26 17:05:24 -0700

    updated HW-2 submission

--- Homeworks/HW-2.py
+++ Homeworks/HW-2.py
@@ -1,210 +1,43 @@
-
 # -*- coding: utf-8 -*-
+
 # Name: Srinivas Avireddy
 # Email: saviredd@eng.ucsd.edu
 # PID: A53101356
 from pyspark import SparkContext
 sc = SparkContext()
-# Your program here
 
-# coding: utf-8
-
-# # Homework 2
-# 
-# In this homework, we are going to play with Twitter data.
-# 
-# The data is represented as rows of of [JSON](https://en.wikipedia.org/wiki/JSON#Example) strings.
-# It consists of [tweets](https://dev.twitter.com/overview/api/tweets), [messages](https://dev.twitter.com/streaming/overview/messages-types), and a small amount of broken data (cannot be parsed as JSON).
-# 
-# For this homework, we will only focus on tweets and ignore all other messages.
-# 
-# # UPDATES
-# 
-# ## Announcement
-# 
-# **We changed the test files size and the corresponding file paths.**
-# 
-# In order to avoid long waiting queue, we decided to limit the input files size for the Playground submissions. Please read the following files to get the input file paths:
-#     * 1GB test: `../Data/hw2-files-1gb.txt`
-#     * 5GB test: `../Data/hw2-files-5gb.txt`
-#     * 20GB test: `../Data/hw2-files-20gb.txt`
-# 
-# **We updated the json parsing section of this notebook.**
-# 
-# Python built-in json library is too slow. In our experiment, 70% of the total running time is spent on parsing tweets. Therefore we recommend using [ujson](https://pypi.python.org/pypi/ujson) instead of json. It is at least 15x faster than the built-in json library according to our tests.
-# 
-# 
-# ## Important Reminders
-# 
-# 1. The tokenizer in this notebook contains UTF-8 characters. So the first line of your `.py` source code must be `# -*- coding: utf-8 -*-` to define its encoding. Learn more about this topic [here](https://www.python.org/dev/peps/pep-0263/).
-# 2. The input files (the tweets) contain UTF-8 characters. So you have to correctly encode your input with some function like `lambda text: text.encode('utf-8')`.
-# 3. `../Data/hw2-files-<param>` may contain multiple lines, one line for one input file. You can use a single textFile call to read multiple files: `sc.textFile(','.join(files))`.
-# 4. The input file paths in `../Data/hw2-files-<param>` contains trailing spaces (newline etc.), which may confuse HDFS if not removed. 
-# 5. Your program will be killed if it cannot finish in 5 minutes. The running time of last 100 submissions (yours and others) can be checked at the "View last 100 jobs" tab. For your information, here is the running time of our solution:
-#    * 1GB test:  53 seconds,
-#    * 5GB test:  60 seconds,
-#    * 20GB test: 114 seconds.
-# 
-# 
-# 
-# 
-# 
-# ## Tweets
-# 
-# A tweet consists of many data fields. [Here is an example](https://gist.github.com/arapat/03d02c9b327e6ff3f6c3c5c602eeaf8b). You can learn all about them in the Twitter API doc. We are going to briefly introduce only the data fields that will be used in this homework.
-# 
-# * `created_at`: Posted time of this tweet (time zone is included)
-# * `id_str`: Tweet ID - we recommend using `id_str` over using `id` as Tweet IDs, becauase `id` is an integer and may bring some overflow problems.
-# * `text`: Tweet content
-# * `user`: A JSON object for information about the author of the tweet
-#     * `id_str`: User ID
-#     * `name`: User name (may contain spaces)
-#     * `screen_name`: User screen name (no spaces)
-# * `retweeted_status`: A JSON object for information about the retweeted tweet (i.e. this tweet is not original but retweeteed some other tweet)
-#     * All data fields of a tweet except `retweeted_status`
-# * `entities`: A JSON object for all entities in this tweet
-#     * `hashtags`: An array for all the hashtags that are mentioned in this tweet
-#     * `urls`: An array for all the URLs that are mentioned in this tweet
-# 
-# 
-# ## Data source
-# 
-# All tweets are collected using the [Twitter Streaming API](https://dev.twitter.com/streaming/overview).
-# 
-# 
-# ## Users partition
-# 
-# Besides the original tweets, we will provide you with a Pickle file, which contains a partition over 452,743 Twitter users. It contains a Python dictionary `{user_id: partition_id}`. The users are partitioned into 7 groups.
-
-# # Part 0: Load data to a RDD
-
-# The tweets data is stored on AWS S3. We have in total a little over 1 TB of tweets. We provide 10 MB of tweets for your local development. For the testing and grading on the homework server, we will use different data.
-# 
-# ## Testing on the homework server
-# In the Playground, we provide three different input sizes to test your program: 1 GB, 10 GB, and 100 GB. To test them, read files list from `../Data/hw2-files-1gb.txt`, `../Data/hw2-files-5gb.txt`, `../Data/hw2-files-20gb.txt`, respectively.
-# 
-# For final submission, make sure to read files list from `../Data/hw2-files-final.txt`. Otherwise your program will receive no points.
-# 
-# ## Local test
-# 
-# For local testing, read files list from `../Data/hw2-files.txt`.
-# Now let's see how many lines there are in the input files.
-# 
-# 1. Make RDD from the list of files in `hw2-files.txt`.
-# 2. Mark the RDD to be cached (so in next operation data will be loaded in memory) 
-# 3. call the `print_count` method to print number of lines in all these files
-# 
-# It should print
-# ```
-# Number of elements: 2193
-# ```
-
-# In[184]:
 
 def print_count(rdd):
     print 'Number of elements:', rdd.count()
 
 
-# In[185]:
-
-#%%time
-# Your code here
-f = open("../Data/hw2-files-20gb.txt","r")
+f = open("../Data/hw2-files-final.txt","r")
 files = [line.strip() for line in f.readlines()]
 RDD  = sc.textFile(",".join(files)).cache()
 print_count(RDD)
-
-
-# # Part 1: Parse JSON strings to JSON objects
-
-# Python has built-in support for JSON.
-# 
-# **UPDATE:** Python built-in json library is too slow. In our experiment, 70% of the total running time is spent on parsing tweets. Therefore we recommend using [ujson](https://pypi.python.org/pypi/ujson) instead of json. It is at least 15x faster than the built-in json library according to our tests.
-
-# In[186]:
-
-import ujson
-
-json_example = '''
-{
-    "id": 1,
-    "name": "A green door",
-    "price": 12.50,
-    "tags": ["home", "green"]
-}
-'''
-
-json_obj = ujson.loads(json_example)
-json_obj
-
-
-# ## Broken tweets and irrelevant messages
-# 
-# The data of this assignment may contain broken tweets (invalid JSON strings). So make sure that your code is robust for such cases.
-# 
-# In addition, some lines in the input file might not be tweets, but messages that the Twitter server sent to the developer (such as [limit notices](https://dev.twitter.com/streaming/overview/messages-types#limit_notices)). Your program should also ignore these messages.
-# 
-# *Hint:* [Catch the ValueError](http://stackoverflow.com/questions/11294535/verify-if-a-string-is-json-in-python)
-# 
-# 
-# (1) Parse raw JSON tweets to obtain valid JSON objects. From all valid tweets, construct a pair RDD of `(user_id, text)`, where `user_id` is the `id_str` data field of the `user` dictionary (read [Tweets](#Tweets) section above), `text` is the `text` data field.
-
-# In[187]:
 
 import ujson
 
 def safe_parse(raw_json):
     try:
         tweet = ujson.loads(raw_json)
-        return (tweet['user']['id_str'],tweet['text'])
+        return (tweet['user']['id_str'],tweet['text'].encode('UTF-8'))
     except:
-        return "error"
+        return 0
 
-users = RDD.map(lambda x: safe_parse(x))            .filter(lambda x: x!="error").cache()
-#users.take(5)
+users = RDD.map(lambda x: safe_parse(x))\
+            .filter(lambda x: x!=0).cache()
 
-
-# (2) Count the number of different users in all valid tweets (hint: [the `distinct()` method](https://spark.apache.org/docs/latest/programming-guide.html#transformations)).
-# 
-# It should print
-# ```
-# The number of unique users is: 2083
-# ```
-
-# In[188]:
+group_RDD = users.map(lambda x: (x[0],1))\
+                 .reduceByKey(lambda x,y: x+y).cache()
 
 def print_users_count(count):
     print 'The number of unique users is:', count
 
+print_users_count(group_RDD.count())
 
-# In[189]:
-
-#get_ipython().run_cell_magic(u'time', u'', u'# your code here\n
-print users.map(lambda x:x[0]).distinct().count()
-
-
-# # Part 2: Number of posts from each user partition
-
-# Load the Pickle file `../Data/users-partition.pickle`, you will get a dictionary which represents a partition over 452,743 Twitter users, `{user_id: partition_id}`. The users are partitioned into 7 groups. For example, if the dictionary is loaded into a variable named `partition`, the partition ID of the user `59458445` is `partition["59458445"]`. These users are partitioned into 7 groups. The partition ID is an integer between 0-6.
-# 
-# Note that the user partition we provide doesn't cover all users appear in the input data.
-
-# (1) Load the pickle file.
-
-# In[191]:
-
-# your code here
 import cPickle as pickle
 partition = pickle.load(open("../Data/users-partition.pickle","rb"))
-
-
-# (2) Count the number of posts from each user partition
-# 
-# Count the number of posts from group 0, 1, ..., 6, plus the number of posts from users who are not in any partition. Assign users who are not in any partition to the group 7.
-# 
-# Put the results of this step into a pair RDD `(group_id, count)` that is sorted by key.
-
-# In[192]:
 
 def find_partition_id(user):
     if user not in partition:
@@ -213,64 +46,15 @@ def find_partition_id(user):
         return partition[user]
 
 
-# In[199]:
-
-# your code here
-users_group = users.map(lambda x: (x[0],find_partition_id(x[0]))).cache()
-#user_group_map = sc.broadcast(users_group.collectAsMap())
-group_RDD = users_group.map(lambda x: (x[1],1))                 .reduceByKey(lambda x,y: x+y)                 .sortByKey(True)
-
-
-# (3) Print the post count using the `print_post_count` function we provided.
-# 
-# It should print
-# 
-# ```
-# Group 0 posted 81 tweets
-# Group 1 posted 199 tweets
-# Group 2 posted 45 tweets
-# Group 3 posted 313 tweets
-# Group 4 posted 86 tweets
-# Group 5 posted 221 tweets
-# Group 6 posted 400 tweets
-# Group 7 posted 798 tweets
-# ```
-
-# In[200]:
-
+group_RDD = group_RDD.map(lambda x: (find_partition_id(x[0]),x[1]))\
+                 .reduceByKey(lambda x,y: x+y)\
+                 .sortByKey(True)
+                
 def print_post_count(counts):
     for group_id, count in counts:
         print 'Group %d posted %d tweets' % (group_id, count)
 
-
-# In[201]:
-
-# your code here
 print_post_count(group_RDD.collect())
-
-
-# # Part 3:  Tokens that are relatively popular in each user partition
-
-# In this step, we are going to find tokens that are relatively popular in each user partition.
-# 
-# We define the number of mentions of a token $t$ in a specific user partition $k$ as the number of users from the user partition $k$ that ever mentioned the token $t$ in their tweets. Note that even if some users might mention a token $t$ multiple times or in multiple tweets, a user will contribute at most 1 to the counter of the token $t$.
-# 
-# Please make sure that the number of mentions of a token is equal to the number of users who mentioned this token but NOT the number of tweets that mentioned this token.
-# 
-# Let $N_t^k$ be the number of mentions of the token $t$ in the user partition $k$. Let $N_t^{all} = \sum_{i=0}^7 N_t^{i}$ be the number of total mentions of the token $t$.
-# 
-# We define the relative popularity of a token $t$ in a user partition $k$ as the log ratio between $N_t^k$ and $N_t^{all}$, i.e. 
-# 
-# \begin{equation}
-# p_t^k = \log \frac{C_t^k}{C_t^{all}}.
-# \end{equation}
-# 
-# 
-# You can compute the relative popularity by calling the function `get_rel_popularity`.
-
-# (0) Load the tweet tokenizer.
-
-# In[202]:
 
 # %load happyfuntokenizing.py
 #!/usr/bin/env python
@@ -472,7 +256,7 @@ class Tokenizer:
                 entnum = ent[2:-1]
                 try:
                     entnum = int(entnum)
-                    s = s.replace(ent, unichr(entnum))	
+                    s = s.replace(ent, unichr(entnum))  
                 except:
                     pass
         # Now the alpha versions:
@@ -486,9 +270,6 @@ class Tokenizer:
                 pass                    
             s = s.replace(amp, " and ")
         return s
-
-
-# In[203]:
 
 from math import log
 
@@ -508,198 +289,51 @@ def print_tokens(tokens, gid = None):
     print
 
 
-# (1) Tokenize the tweets using the tokenizer we provided above named `tok`. Count the number of mentions for each tokens regardless of specific user group.
-# 
-# Call `print_count` function to show how many different tokens we have.
-# 
-# It should print
-# ```
-# Number of elements: 8949
-# ```
-
-# In[204]:
-
 def user_tokenize(tokens,user):
     token_list = []
     for token in tokens:
         token_list.append((token,user))
     return token_list
-        
-    
 
-
-# In[205]:
-
-#get_ipython().run_cell_magic(u'time', u'', 
 unique_user_tokens = users.map(lambda x:(tok.tokenize(x[1]),x[0]))\
-             .flatMap(lambda x:user_tokenize(x[0],x[1]))\
-             .distinct()
+              .flatMap(lambda x:user_tokenize(x[0],x[1]))\
+              .distinct()
+#print unique_user_tokens.reduceByKey(lambda x,y:x+y).take(10)
 unique_user_tokens_grouped = unique_user_tokens.groupByKey().cache()
 print_count(unique_user_tokens_grouped)
 
-
-
-# (2) Tokens that are mentioned by too few users are usually not very interesting. So we want to only keep tokens that are mentioned by at least 100 users. Please filter out tokens that don't meet this requirement.
-# 
-# Call `print_count` function to show how many different tokens we have after the filtering.
-# 
-# Call `print_tokens` function to show top 20 most frequent tokens.
-# 
-# It should print
-# ```
-# Number of elements: 44
-# ===== overall =====
-# :	1388.0000
-# rt	1237.0000
-# .	826.0000
-# …	673.0000
-# the	623.0000
-# trump	582.0000
-# to	499.0000
-# ,	489.0000
-# a	404.0000
-# is	376.0000
-# in	297.0000
-# of	292.0000
-# and	288.0000
-# for	281.0000
-# !	269.0000
-# ?	210.0000
-# on	195.0000
-# i	192.0000
-# you	191.0000
-# this	190.0000
-# ```
-
-# In[206]:
-
-#%%time
 filtered_tokens = unique_user_tokens_grouped.filter(lambda x: len(x[1]) >= 100).cache()
 
-sorted_filtered_tokens_count = filtered_tokens.map(lambda x: (len(x[1]),x[0]))                                    .sortByKey(False)                                    .map(lambda (b,a):(a.encode('UTF-8'),b))
+'''
+sorted_filtered_tokens_count = filtered_tokens.map(lambda x: (len(x[1]),x[0]))\
+                                    .sortByKey(False)\
+                                    .map(lambda (b,a):(a.encode('UTF-8'),b))
+'''
+
+sorted_filtered_tokens_count = filtered_tokens.map(lambda x: ((-len(x[1]),x[0]),x[0]))\
+                                   .sortByKey(True)\
+                                   .map(lambda x:(x[1].encode('UTF-8'),-x[0][0]))
 print_count(sorted_filtered_tokens_count)
 print_tokens(sorted_filtered_tokens_count.take(20))
-                
 
 
-
-# (3) For all tokens that are mentioned by at least 100 users, compute their relative popularity in each user group. Then print the top 10 tokens with highest relative popularity in each user group. In case two tokens have same relative popularity, break the tie by printing the alphabetically smaller one.
-# 
-# **Hint:** Let the relative popularity of a token $t$ be $p$. The order of the items will be satisfied by sorting them using (-p, t) as the key.
-# 
-# It should print
-# ```
-# ===== group 0 =====
-# ...	-3.5648
-# at	-3.5983
-# hillary	-4.0875
-# i	-4.1255
-# bernie	-4.1699
-# not	-4.2479
-# https	-4.2695
-# he	-4.2801
-# in	-4.3074
-# are	-4.3646
-# 
-# ===== group 1 =====
-# #demdebate	-2.4391
-# -	-2.6202
-# &	-2.7472
-# amp	-2.7472
-# clinton	-2.7570
-# ;	-2.7980
-# sanders	-2.8838
-# ?	-2.9069
-# in	-2.9664
-# if	-3.0138
-# 
-# ===== group 2 =====
-# are	-4.6865
-# and	-4.7105
-# bernie	-4.7549
-# at	-4.7682
-# sanders	-4.9542
-# that	-5.0224
-# in	-5.0444
-# donald	-5.0618
-# a	-5.0732
-# #demdebate	-5.1396
-# 
-# ===== group 3 =====
-# #demdebate	-1.3847
-# bernie	-1.8480
-# sanders	-2.1887
-# of	-2.2356
-# that	-2.3785
-# the	-2.4376
-# …	-2.4403
-# clinton	-2.4467
-# hillary	-2.4594
-# be	-2.5465
-# 
-# ===== group 4 =====
-# hillary	-3.7395
-# sanders	-3.9542
-# of	-4.0199
-# clinton	-4.0790
-# at	-4.1832
-# in	-4.2143
-# a	-4.2659
-# on	-4.2854
-# .	-4.3681
-# the	-4.4251
-# 
-# ===== group 5 =====
-# cruz	-2.3861
-# he	-2.6280
-# are	-2.7796
-# will	-2.7829
-# the	-2.8568
-# is	-2.8822
-# for	-2.9250
-# that	-2.9349
-# of	-2.9804
-# this	-2.9849
-# 
-# ===== group 6 =====
-# @realdonaldtrump	-1.1520
-# cruz	-1.4532
-# https	-1.5222
-# !	-1.5479
-# not	-1.8904
-# …	-1.9269
-# will	-2.0124
-# it	-2.0345
-# this	-2.1104
-# to	-2.1685
-# 
-# ===== group 7 =====
-# donald	-0.6422
-# ...	-0.7922
-# sanders	-1.0282
-# trump	-1.1296
-# bernie	-1.2106
-# -	-1.2253
-# you	-1.2376
-# clinton	-1.2511
-# if	-1.2880
-# i	-1.2996
-# ```
-
-# In[209]:
-user_group_map = sc.broadcast(users_group.collectAsMap())
 def token_group(token):
-    group_list = map(lambda x: user_group_map.value[x], token[1])
-    return zip([token[0]] * len(token[1]), group_list, [len(token[1])] * len(token[1]))
-    
+    group_tuples = []
+    length = len(token[1])
+    for user in token[1]:
+        group_tuples.append((token[0],find_partition_id(user),length))
+    return group_tuples
 
-group_tokens = filtered_tokens.flatMap(lambda x: token_group(x)).cache()
-#print group_tokens.collect()
+
+group_tokens = filtered_tokens.flatMap(lambda x:token_group(x)).map(lambda x:((x[0],x[1],x[2]),1))\
+                              .reduceByKey(lambda x,y: x+y)\
+                                .map(lambda x:(((-get_rel_popularity(x[1],x[0][2]),x[0][0]),(x[0][0],x[0][1]))))\
+                                .sortByKey(True)\
+                                 .map(lambda x: ((x[1][0].encode('UTF-8'),-x[0][0],x[1][1])))
 for i in xrange(0,8):
-    reduce_rdd = group_tokens.filter(lambda x: x[1] == i).map(lambda x:((x[0],x[2]),1)).reduceByKey(lambda x,y:x+y)\
-                         .map(lambda x:((-get_rel_popularity(x[1],x[0][1]),x[0][0]),x[0][0]))\
-                         .sortByKey(True)\
-                         .map(lambda x: (x[1].encode('UTF-8'),-x[0][0]))
-    #print reduce_rdd.first()
+    reduce_rdd = group_tokens.filter(lambda x: x[2] == i).map(lambda x:(x[0],x[1]))
+    #print reduce_rdd.take(5)
     print_tokens(reduce_rdd.take(10),i)
-#get_ipython().run_cell_magic(u'time', u'', u"user_group_map = sc.broadcast(users_group.collectAsMap())\ndef token_group(token):\n    group_list = map(lambda x: user_group_map.value[x], token[1])\n    return zip([token[0]] * len(token[1]), group_list, [len(token[1])] * len(token[1]))\n    \n\ngroup_tokens = filtered_tokens.flatMap(lambda x: token_group(x)).cache()\n#print group_tokens.collect()\nfor i in xrange(0,8):\n    reduce_rdd = group_tokens.filter(lambda x: x[1] == i).map(lambda x:((x[0],x[2]),1)).reduceByKey(lambda x,y:x+y)\\\n                         .map(lambda x:((-get_rel_popularity(x[1],x[0][1]),x[0][0]),x[0][0]))\\\n                         .sortByKey(True)\\\n                         .map(lambda x: (x[1].encode('UTF-8'),-x[0][0]))\n    #print reduce_rdd.first()\n    print_tokens(reduce_rdd.take(10),i)")
+#group_tokens.take(5)
+
+
